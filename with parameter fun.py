b5153df34798e1c70a5d4b2529e57152f89212e0
sanyogitacharegaonkar <129272778+sanyogitacharegaonkar@users.noreply.github.com>
2023-07-12 13:37:15 +0530

    Add files via upload

---
+++ with parameter fun.py
@@ -0,0 +1,9 @@
+def disp(name="riya",rn=50):
+    print("name:",name)
+    print("rollno:",rn)
+name=input("enter name:")
+rn=int(input("enter rollno:"))
+disp(name,rn)
+disp("disha",5601)
+disp()
+disp("abc")
