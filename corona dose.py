f0f4a50358549229cbb4ee4ac707ddddf9419542
sanyogitacharegaonkar <129272778+sanyogitacharegaonkar@users.noreply.github.com>
2023-07-12 13:06:32 +0530

    Add files via upload

---
+++ corona dose.py
@@ -0,0 +1,10 @@
+print("vaccination information:")
+dose=int(input("enter the number of dose coplited:"))
+if(dose==1):
+    print("please complite your 2 dose")
+elif(dose==2):
+    print("please complite your booster dose")
+elif(dose==3):
+    print("your all dose are complited.now you are safe very nice")
+else:
+    print("please get vaccination for your safty")
