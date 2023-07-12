1a7af35aae55a82d6ba00b006be37ae7e2fd52c5
sanyogitacharegaonkar <129272778+sanyogitacharegaonkar@users.noreply.github.com>
2023-07-12 13:35:23 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+t1=(10,20,"abc","pqr",10.03)
+t2=(10,20,5.3,2,15)
+print(t1)
+print(type(t1))
+print(t2)
+print(type(t2))
+t3=10,20
+t4=(10, )
+t5=()
+print(type(t3))
+print(type(t4))
+print(type(t5))
+
