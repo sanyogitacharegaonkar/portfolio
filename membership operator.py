cefa1e05e903dc659dfa3c1ebfa64e8bdf5200ea
sanyogitacharegaonkar <129272778+sanyogitacharegaonkar@users.noreply.github.com>
2023-07-12 13:23:09 +0530

    Add files via upload

---
+++ membership operator.py
@@ -0,0 +1,9 @@
+l1=[10,20,30,40.03,"abc","pqr"]
+print("10 in l1:",10 in l1)
+print("10 not in l1:",10 not in l1)
+print("50 in l1:",50 in l1)
+print("50 not in l1:",50 not in l1)
+print("abc in l1:",'abc' in l1)
+print("anc not in l1:",'abc' not in l1)
+print("pqr in l1:",'pqr' in l1)
+print("pqr not in l1:",'pqr' not in l1)
