40fa4c3c3645a493cb74118bec07f91de5f5e4d1
sanyogitacharegaonkar <129272778+sanyogitacharegaonkar@users.noreply.github.com>
2023-07-12 13:30:49 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,8 @@
+no1=int(input("enter a no1:"))
+no2=int(input("enter a no2:"))
+if(no1>no2):
+    print(no1,"is greatest")
+elif(no1==no2):
+    print(no1,"is equal to",no2)
+else:
+    print(no1,"is smallest")
