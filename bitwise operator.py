e6e6838ff99f254c84a4e4bbeac7071c30e47fe8
sanyogitacharegaonkar <129272778+sanyogitacharegaonkar@users.noreply.github.com>
2023-07-12 13:08:21 +0530

    Add files via upload

---
+++ bitwise operator.py
@@ -0,0 +1,14 @@
+a=0
+b=1
+print("bitwise operator")
+print("(a&b)=",(a&b))
+print("(a&a)=",(a&a))
+print("(b&b)=",(b&b))
+print("(a|b)=",(a|b))
+print("(a|a)=",(a|a))
+print("(b|b)=",(b|b))
+print("(a^b)=",(a^b))
+print("(a^a)=",(a^a))
+print("(b^b)=",(b^b))
+print("(!a)=",(a))
+print("(!b)=",(b))
