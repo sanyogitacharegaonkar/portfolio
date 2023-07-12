0be84bdd300ce86023622f3de5118e2cf1f5d192
sanyogitacharegaonkar <129272778+sanyogitacharegaonkar@users.noreply.github.com>
2023-07-12 13:08:04 +0530

    Add files via upload

---
+++ assingment operator.py
@@ -0,0 +1,16 @@
+a=int(input("enter a no1:"))
+b=int(input("enter a no2:"))
+a+=b
+print("(a+=b)=",a)
+a-=b
+print("(a-=b)=",a)
+a*=b
+print("(a*=b)=",a)
+a/=b
+print("(a/=b)=",a)
+a%=b
+print("(a%=b)=",a)
+a**=b
+print("(a**=b)=",a)
+a//=b
+print("(a//=b)=",a)
