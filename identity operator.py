bd473d6a83b82ee1682742d504c448c4c53301ef
sanyogitacharegaonkar <129272778+sanyogitacharegaonkar@users.noreply.github.com>
2023-07-12 13:15:41 +0530

    Add files via upload

---
+++ identity operator.py
@@ -0,0 +1,11 @@
+l1=[10,20,30]
+l2=[10,20,30]
+a=10
+b=20
+c=10
+print("l1 is l2:",l1 is l2)
+print("l1 is not l2:",l1 is not l2)
+print("a is b:",a is b)
+print("a is not  b:",a is not b)
+print("a is c:",a is c)
+print("a is not  c:",a is not c)
